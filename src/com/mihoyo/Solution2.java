e4c9d6f5e889853a6b6ed6660b200433147bb876
liar1573 <894993478@qq.com>
2020-04-06 10:19:37 +0800

    某种树的结构判断-米忽悠2020春招（实习）笔试题2

---
+++ src/com/mihoyo/Solution2.java
@@ -0,0 +1,30 @@
+package com.mihoyo;
+
+public class Solution2 {
+	 /**
+     * 给定一个01组成的字符串计算出01串表示的二叉树中最少无重复覆盖监控节点数
+     * @param inputs string字符串 01组成的字符串
+     * @return int整型
+     */
+	
+	/**
+	 * 下方备注说明了01串是按照BFS广度搜索方式显示的二叉树（不一定是满二叉树），
+	 * 标记为0的节点没有子节点
+	 * 注意到这里的输入并不是根节点表示的树，而是根据BFS规则和自定义规则的01字符串
+	 * 某种程度上可能还要根据01字符串生成树，才方便进行判断
+	 * 有无可能直接通过数学规律进行判断不转换成树？？
+	 * 满足全监督且不重复的树应该有其自身的规律才对
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+   public int min_monitors (String inputs) {
+       // write code here
+	   return 0;
+   }
+
+}
