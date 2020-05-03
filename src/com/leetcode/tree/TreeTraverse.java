bd25509c917bdb20c39a05517a837a39271a5ceb
liar1573 <894993478@qq.com>
2020-05-03 14:56:41 +0800

    工具类，写了一些树的常用遍历方法，便于以后做题时直接调试使用

---
+++ src/com/leetcode/tree/TreeTraverse.java
@@ -0,0 +1,53 @@
+/**TreeTraverse.java
+ * com.leetcode.tree
+ * TODO
+ * 写一些常用的树遍历方法，便于以后调试使用
+ * @author liar
+ * 2020年4月22日 上午11:05:28
+ * @version 1.0
+ */
+package com.leetcode.tree;
+
+
+public class TreeTraverse {
+	
+	public static void preOrderTraverse(TreeNode root) {
+		if(root == null)
+			return;
+		
+		printIntValue(root);
+		preOrderTraverse(root.left);
+		preOrderTraverse(root.right);
+	}
+	
+	public static void inOrderTraverse(TreeNode root) {
+		if(root == null)
+			return;
+		
+		inOrderTraverse(root.left);
+		printIntValue(root);
+		inOrderTraverse(root.right);
+	}
+	
+	public static void lastOrderTraverse(TreeNode root) {
+		if(root == null)
+			return;
+		
+		lastOrderTraverse(root.left);
+		lastOrderTraverse(root.right);
+		printIntValue(root);
+	}
+	
+	
+	public static void printIntValue(TreeNode node) {
+		System.out.println(node.val);
+	}
+	
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
