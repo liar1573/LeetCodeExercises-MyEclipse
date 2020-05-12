58ae4b4052a83989f4e82cc1109763d131d3661c
liar1573 <894993478@qq.com>
2020-05-12 11:09:38 +0800

    便于以后测试二叉树问题，稍微调整了包内类结构

---
+++ src/com/leetcode/tree/TreeNode.java
@@ -0,0 +1,17 @@
+/**TreeNode.java
+ * com.leetcode.tree
+ * TODO
+ * 最常见的TreeNode结构，用于同一导入调试
+ * @author liar
+ * 2020年5月12日 上午10:45:49
+ * @version 1.0
+ */
+package com.leetcode.tree;
+
+
+public class TreeNode {
+	int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
