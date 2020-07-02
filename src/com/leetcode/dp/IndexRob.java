09c49040b30df97c964afdb4e08e12c5dad180d6
liar1573 <894993478@qq.com>
2020-07-02 13:17:49 +0800

    198打家劫舍基本模型，补充了简单递归解法和带备忘录的递归解法，使得思路更完整。

--- src/com/leetcode/dp/IndexRob.java
+++ src/com/leetcode/dp/IndexRob.java
@@ -37,5 +37,33 @@ public class IndexRob {
 
         return result;
     }
+	
+	public int dfsRob(int[] nums, int index) {
+		if(index >= nums.length)  return 0;
+		if(index >= nums.length - 1)  return nums[nums.length - 1];
+		
+		int max1 = dfsRob(nums, index + 1);
+		int max2 = dfsRob(nums, index + 2) + nums[index];
+		
+		return (max1 > max2) ? max1 : max2;
+	}
+	
+	public int dfsRobWithMemo(int[] nums, int index, int[] memo) {
+		if(index >= nums.length)  return 0;
+		if(index >= nums.length - 1){
+			memo[index] = nums[nums.length - 1];
+			return nums[nums.length - 1];
+		}
+		
+		if(memo[index] >= 0)
+			return memo[index];
+		
+		
+		int max1 = dfsRobWithMemo(nums, index + 1, memo);
+		int max2 = dfsRobWithMemo(nums, index + 2, memo) + nums[index];
+		
+		memo[index] = (max1 > max2) ? max1 : max2;
+		return memo[index];
+	}
 
 }
